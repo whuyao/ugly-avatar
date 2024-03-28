439bd6f296adf8e61643885e6deee8c134add1f1
Yao <whuyao@foxmail.com>
2024-03-28 19:04:45 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/ugly-avatar/'
+  ? '/ugly-avatar-10m/'
   : '/'
 })
